57be26a921ba0dc671b446033716314d72474679
enter-the-code <ssoonngg53@naver.com>
2025-05-12 03:23:11 +0900

    add git hub link

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,21 @@
-import { Box, Button, Card, Center, FileUpload, Heading, Icon, CheckboxGroup, Flex, Text, Separator, ButtonGroup, Group, Input, Tabs } from "@chakra-ui/react";
-import { LuUpload, LuUser, LuFolder } from "react-icons/lu";
+import {
+  Box,
+  Button,
+  Card,
+  Center,
+  FileUpload,
+  Heading,
+  Icon,
+  CheckboxGroup,
+  Flex,
+  Text,
+  Separator,
+  ButtonGroup,
+  Group,
+  Input,
+  Tabs,
+} from "@chakra-ui/react";
+import { LuUpload, LuFolder, LuGithub } from "react-icons/lu";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Default from "~/config/Default";
@@ -10,160 +26,234 @@ import Confirm from "~/pages/confirm";
 import { AnimationBox } from "~/common/AnimationBox";
 import { throttle } from "lodash";
 const MainPage = () => {
-    const [file, setFiles] = useState<File>();
-    const [checkElementList, setCheckElementList] = useState<CheckElementList[]>(Default.CheckElementList);
-    const [mode, setMode] = useState<ButtonMode>("upload");
-    const navigate = useNavigate();
-    const [requestId, setRequestId] = useState<string>("");
-    const [initId, setInitId] = useState<string>("");
+  const [file, setFiles] = useState<File>();
+  const [checkElementList, setCheckElementList] = useState<CheckElementList[]>(
+    Default.CheckElementList
+  );
+  const [mode, setMode] = useState<"upload" | "token" | "github">("upload");
+  const navigate = useNavigate();
+  const [requestId, setRequestId] = useState<string>("");
+  const [initId, setInitId] = useState<string>("");
+  const [githubLink, setGithubLink] = useState<string>("");
 
-    
-    
-    const requestThrottle = throttle(async (file: File, checkElementList: CheckElementList[]) => {
-        const requestId = await postFileByRequestId(file, checkElementList);
-        setInitId(requestId);
-    }, 2000);
-    
-    const requestFileUpload = async () => {
-        try {
-            if (!file) throw new Error("No file selected");
-            requestThrottle(file, checkElementList);
-        } catch (error) {
-            console.error(error);
-        }
+  const requestThrottle = throttle(
+    async (
+      file: File | undefined,
+      checkElementList: CheckElementList[],
+      githubLink: string
+    ) => {
+      const requestId = await postFileByRequestId(
+        file,
+        checkElementList,
+        githubLink
+      );
+      setInitId(requestId);
+    },
+    2000
+  );
+
+  const requestFileUpload = async () => {
+    try {
+      if (!file && mode === "upload") throw new Error("No file selected");
+      requestThrottle(file, checkElementList, githubLink);
+    } catch (error) {
+      console.error(error);
     }
-    
-    const handleFileUpload = (file: File) => {
-        setFiles(file);
-    }
-    
-    const handleCheckElementList = (details: string[]) => {
-        console.log("details", details);
-        const newCheckElementList = Default.CheckBoxAllList
-        .filter(checkList => details.includes(checkList.id.toString()))
-        .map(checkList => ({
-            id: checkList.id.toString(),
-            name: checkList.name
-        }));
-        console.log("newCheckElementList", newCheckElementList);
-        setCheckElementList(newCheckElementList);
-        console.log("checkElementList", checkElementList);
-    }
-    
-    useEffect(() => {
-        setFiles(undefined);
-        setCheckElementList(Default.CheckElementList);
-        setInitId("");
-    }, [mode]);
-    
-    console.log("defcheckElementList", checkElementList);
-    console.log("111defcheckElementList", Default.CheckBoxElementListObject);
-    
-    return initId === "" ? (
-        <Center w="100%" h="100%">
-            <AnimationBox dataState="open" animationName="slide-from-bottom , fade-in" animationDuration="500ms">
-                <ButtonGroup m="10px" ml="0px" size="lg" variant="outline">
-                    <Tabs.Root defaultValue="upload" variant="plain" onValueChange={(details) => {
-                        console.log(details);
-                        setMode(details.value as ButtonMode);
-                        console.log(mode);
-                    }}>
-                        <Tabs.List bg="bg.muted" rounded="l3" p="1">
-                            <Tabs.Trigger value="upload">
-                                <LuUser />
-                                파일 업로드
-                            </Tabs.Trigger>
-                            <Tabs.Trigger value="result">
-                                <LuFolder />
-                                결과 창보기(요청 아이디)
-                            </Tabs.Trigger>
-                            <Tabs.Indicator rounded="l2" />
-                        </Tabs.List>
-                    </Tabs.Root>
-                </ButtonGroup>
-                <Card.Root w="800px" variant={"elevated"}>
-                    <Card.Header>
-                        <Center>
-                            {mode === "upload" ? <Heading>프로젝트 업로드</Heading> : <Heading>결과 창보기(토큰 입력)</Heading>}
-                        </Center>
-                    </Card.Header>
-                    {mode === "upload" ? (
-                        <Card.Body>
-                            <FileUpload.Root alignItems="stretch" maxFiles={1} onFileChange={(details) => handleFileUpload(details.acceptedFiles[0])}>
-                                <FileUpload.HiddenInput />
-                                <FileUpload.Dropzone>
-                                    <Icon size="md" color="fg.muted">
-                                        <LuUpload />
-                                    </Icon>
-                                    <FileUpload.DropzoneContent>
-                                        <Box>Drag and drop files here</Box>
-                                        <Box color="fg.muted">.zip up to 3GB</Box>
-                                    </FileUpload.DropzoneContent>
-                                </FileUpload.Dropzone>
-                                <FileUpload.List />
-                            </FileUpload.Root>
-                        </Card.Body>
-                    ) : (
-                        <Card.Body p="50px">
-                            <Center>
-                                <Group attached w="full" maxW="lg">
-                                    <Input flex="" placeholder="Enter your request Token" onChange={(e) => setRequestId(e.target.value)} />
-                                    <Button onClick={() => {
-                                        navigate(`/${requestId}/result`);
-                                    }} ml="10px" bg="bg.subtle" variant="outline">
-                                        결과 창 보기
-                                    </Button>
-                                </Group>
-                            </Center>
-                        </Card.Body>
-                    )}
-                    {mode === "upload" && (
-                        <>
-                            <Card.Footer flexDirection="column" w="full">
-                                {Object.entries(Default.CheckBoxElementListObject).map(([_, value], index: number) => (
-                                    <React.Fragment key={index}>
-                                        <Flex ml="5%" w="full" flexDirection="Row" gap="12">
-                                            <Text alignSelf="center" w="12%">코드 품질</Text>
-                                            <Separator orientation="vertical" />
-                                            <CheckboxGroup  w="90%"
-                                                value={checkElementList.map((checkElement) => {
-                                                    console.log("11111checkElement", checkElement);
-                                                    return checkElement.id.toString();
-                                                })}
-                                                onValueChange={handleCheckElementList}
-                                                flexDirection="Row"
-                                                gap="6"
-                                                display="flex"
-                                                flexWrap="wrap"
-                                            >
-                                                {value.map((checkList) => (
-                                                    <Checkbox key={checkList.id} value={(checkList.id).toString()}>
-                                                        {checkList.name}
-                                                    </Checkbox>
-                                                ))}
-                                            </CheckboxGroup>
-                                        </Flex>
-                                        {index !== 3 && <Separator mt="10px" w="full" orientation="horizontal" />}
-                                    </React.Fragment>
-                                ))}
-                            </Card.Footer>
-                            <Button
-                                variant={"subtle"}
-                                colorPalette={"black"}
-                                onClick={() => {
-                                    requestFileUpload()
-                                }}
+  };
+
+  const handleFileUpload = (file: File) => {
+    setFiles(file);
+  };
+
+  const handleCheckElementList = (details: string[]) => {
+    console.log("details", details);
+    const newCheckElementList = Default.CheckBoxAllList.filter((checkList) =>
+      details.includes(checkList.id.toString())
+    ).map((checkList) => ({
+      id: checkList.id.toString(),
+      name: checkList.name,
+    }));
+    console.log("newCheckElementList", newCheckElementList);
+    setCheckElementList(newCheckElementList);
+    console.log("checkElementList", checkElementList);
+  };
+
+  useEffect(() => {
+    setFiles(undefined);
+    setCheckElementList(Default.CheckElementList);
+    setInitId("");
+    setGithubLink("");
+  }, [mode]);
+
+  console.log("defcheckElementList", checkElementList);
+  console.log("111defcheckElementList", Default.CheckBoxElementListObject);
+
+  return initId === "" ? (
+    <Center w="100%" h="100%">
+      <AnimationBox
+        dataState="open"
+        animationName="slide-from-bottom , fade-in"
+        animationDuration="500ms"
+      >
+        <ButtonGroup m="10px" ml="0px" size="lg" variant="outline">
+          <Tabs.Root
+            defaultValue="upload"
+            variant="plain"
+            onValueChange={(details) => {
+              console.log(details);
+              setMode(details.value as "upload" | "token" | "github");
+              console.log(mode);
+            }}
+          >
+            <Tabs.List bg="bg.muted" rounded="l3" p="1">
+              <Tabs.Trigger value="upload">
+                <LuUpload />
+                파일 업로드
+              </Tabs.Trigger>
+              <Tabs.Trigger value="github">
+                <LuGithub />
+                깃헙 링크
+              </Tabs.Trigger>
+              <Tabs.Trigger value="result">
+                <LuFolder />
+                결과 창보기(요청 아이디)
+              </Tabs.Trigger>
+              <Tabs.Indicator rounded="l2" />
+            </Tabs.List>
+          </Tabs.Root>
+        </ButtonGroup>
+        <Card.Root w="800px" variant={"elevated"}>
+          <Card.Header>
+            <Center>
+              {mode === "upload" ? (
+                <Heading>프로젝트 업로드</Heading>
+              ) : mode === "github" ? (
+                <Heading>GitHub 링크</Heading>
+              ) : (
+                <Heading>결과 창보기(토큰 입력)</Heading>
+              )}
+            </Center>
+          </Card.Header>
+          {mode === "upload" ? (
+            <Card.Body>
+              <FileUpload.Root
+                alignItems="stretch"
+                maxFiles={1}
+                onFileChange={(details) =>
+                  handleFileUpload(details.acceptedFiles[0])
+                }
+              >
+                <FileUpload.HiddenInput />
+                <FileUpload.Dropzone>
+                  <Icon size="md" color="fg.muted">
+                    <LuUpload />
+                  </Icon>
+                  <FileUpload.DropzoneContent>
+                    <Box>Drag and drop files here</Box>
+                    <Box color="fg.muted">.zip up to 3GB</Box>
+                  </FileUpload.DropzoneContent>
+                </FileUpload.Dropzone>
+                <FileUpload.List />
+              </FileUpload.Root>
+            </Card.Body>
+          ) : (
+            <Card.Body p="50px">
+              <Center>
+                <Group attached w="full" maxW="lg">
+                  {mode === "github" ? (
+                    <Input
+                      flex=""
+                      placeholder="input github link"
+                      onChange={(e) => setGithubLink(e.target.value)}
+                    />
+                  ) : (
+                    <Input
+                      flex=""
+                      placeholder="input request token"
+                      onChange={(e) => setRequestId(e.target.value)}
+                    />
+                  )}
+                  <Button
+                    onClick={() => {
+                      if (mode === "github") {
+                        requestFileUpload();
+                      } else {
+                        navigate(`/${requestId}/result`);
+                      }
+                    }}
+                    ml="10px"
+                    bg="bg.subtle"
+                    variant="outline"
+                  >
+                    {mode === "github" ? "링크 업로드" : "결과 창 보기"}
+                  </Button>
+                </Group>
+              </Center>
+            </Card.Body>
+          )}
+          {(mode === "upload" || mode === "github") && (
+            <>
+              <Card.Footer mt="20px" flexDirection="column" w="full">
+                {Object.entries(Default.CheckBoxElementListObject).map(
+                  ([_, value], index: number) => (
+                    <React.Fragment key={index}>
+                      <Flex ml="5%" w="full" flexDirection="Row" gap="12">
+                        <Text alignSelf="center" w="12%">
+                          코드 품질
+                        </Text>
+                        <Separator orientation="vertical" />
+                        <CheckboxGroup
+                          w="90%"
+                          value={checkElementList.map((checkElement) => {
+                            console.log("11111checkElement", checkElement);
+                            return checkElement.id.toString();
+                          })}
+                          onValueChange={handleCheckElementList}
+                          flexDirection="Row"
+                          gap="6"
+                          display="flex"
+                          flexWrap="wrap"
+                        >
+                          {value.map((checkList) => (
+                            <Checkbox
+                              key={checkList.id}
+                              value={checkList.id.toString()}
                             >
-                                업로드
-                            </Button>
-                        </>
-                    )}
-                </Card.Root>
-            </AnimationBox>
-        </Center>
-    ) : (
-        <Confirm requestId={initId as string} />
-    );
+                              {checkList.name}
+                            </Checkbox>
+                          ))}
+                        </CheckboxGroup>
+                      </Flex>
+                      {/* {index !== 0 && (
+                        <Separator
+                          mt="10px"
+                          w="full"
+                          orientation="horizontal"
+                        />
+                      )} */}
+                    </React.Fragment>
+                  )
+                )}
+              </Card.Footer>
+              {mode === "upload" && (
+                <Button
+                  variant={"subtle"}
+                  colorPalette={"black"}
+                  onClick={() => {
+                    requestFileUpload();
+                  }}
+                >
+                  업로드
+                </Button>
+              )}
+            </>
+          )}
+        </Card.Root>
+      </AnimationBox>
+    </Center>
+  ) : (
+    <Confirm requestId={initId as string} />
+  );
 };
 
 export default MainPage;
--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,148 +1,165 @@
-import { Box, Card, Center, Spinner, IconButton, Breadcrumb, Text, Icon, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  Center,
+  Spinner,
+  IconButton,
+  Breadcrumb,
+  Text,
+  Icon,
+  Heading,
+} from "@chakra-ui/react";
 import { useCallback, useState, useEffect } from "react";
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 import { LuDownload, LuHouse } from "react-icons/lu";
 import { CiWarning } from "react-icons/ci";
 import { useParams } from "react-router-dom";
 import getResultByRequestId from "~/api/result/getResultByRequestId";
 import { AnimationBox } from "~/common/AnimationBox";
 const ResultPage = () => {
-
-    const requestId = useParams().requestId;
-    const [content, setContent] = useState<Blob>();
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [isDownload, setIsDownload] = useState<boolean>(false);
-    const [retryFlag, setRetryFlag] = useState<boolean>(false);
-    const Init = useCallback(async () => {
-        try {
+  const requestId = useParams().requestId;
+  const [content, setContent] = useState<Blob>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isDownload, setIsDownload] = useState<boolean>(false);
+  const [retryFlag, setRetryFlag] = useState<boolean>(false);
+  const Init = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      if (requestId) {
+        let result: Blob;
+        let status: string;
+        [result, status] = await getResultByRequestId(requestId);
+        const MAX_RETRIES = 2;
+        let retryCount = 0;
+        const checkStatus = async () => {
+          if (retryCount > MAX_RETRIES) {
             setIsLoading(true);
-            if (requestId) {
-                let result: Blob;
-                let status: string;
-                [result, status] = await getResultByRequestId(requestId);
-                const MAX_RETRIES = 10;
-                let retryCount = 0;
-                const checkStatus = async () => {
-                    if (status === "running" && retryCount < MAX_RETRIES) {
-                        [result, status] = await getResultByRequestId(requestId);
-                        setContent(result);
-                        retryCount++;
-                        setTimeout(checkStatus, 3000);
-                    } else {
-                        setIsLoading(false);
-                    }
-                };
+            setRetryFlag(true);
+            return;
+          }
+          if (status === "running") {
+            [result, status] = await getResultByRequestId(requestId);
+            setContent(result);
+            retryCount++;
+            setTimeout(checkStatus, 3000);
+          } else if (status === "done") {
+            setIsLoading(false);
+          } else if (status === "failed") {
+            setIsLoading(false);
+            setRetryFlag(true);
+            setContent(undefined);
+          }
+        };
 
-                if (status !== "done") {
-                    setTimeout(checkStatus, 3000);
-                }
-                if (retryCount >= MAX_RETRIES) {
-                    setIsLoading(true);
-                    setRetryFlag(true);
-                }
-            }
-        } catch (error) {
-            console.error(error);
-            // navigate("/");
-
-        } finally {
+        if (status !== "done") {
+          setTimeout(checkStatus, 3000);
         }
-    }, [requestId]);
-
-    const handleDownload = async () => {
-        try {
-            setIsDownload(true);
-            saveAs(content as Blob, `${requestId}.pdf`);
-            setIsDownload(false);
-        } catch (error) {
-            setIsDownload(false);
-            console.error(error);
-        }
+      }
+    } catch (error) {
+      console.error(error);
+      // navigate("/");
+    } finally {
     }
+  }, [requestId]);
 
-    useEffect(() => {
-        //window.location.replace("/")
-        Init();
-    }, [Init]);
+  const handleDownload = async () => {
+    try {
+      setIsDownload(true);
+      saveAs(content as Blob, `${requestId}.pdf`);
+      setIsDownload(false);
+    } catch (error) {
+      setIsDownload(false);
+      console.error(error);
+    }
+  };
 
-    return (
-        <>
-            <Box
-                mt={10}
-                pb={10}
-                minH="100vh"
-                display="flex"
-                flexDirection="column"
-            >
-                <Breadcrumb.Root h="30px" w="90%" mx="auto">
-                    <Breadcrumb.List>
-                        <Breadcrumb.Item>
-                            <Breadcrumb.Link href="/">
-                                <LuHouse size="25px" />
-                                <Text fontSize="17px">Home</Text>
-                            </Breadcrumb.Link>
-                        </Breadcrumb.Item>
-                    </Breadcrumb.List>
-                </Breadcrumb.Root>
-                <Center w="full">
-                    {isLoading ? (
-                        <Center w="full">
-                            <AnimationBox w={"90%"} dataState="open" animationName="slide-from-bottom , fade-in" animationDuration="500ms">
-                                <Card.Root variant={"elevated"}>
-                                    <Center p={10}>
-                                        {retryFlag ? (
-                                            <Text>
-                                                시간이 걸립니다 잠시후에 다시 시도 부탁드립니다
-                                            </Text>
-                                        ) : (
-                                            <Spinner size={"xl"} />
-                                        )}
-                                    </Center>
-                                </Card.Root>
-                            </AnimationBox>
-                        </Center>
+  useEffect(() => {
+    //window.location.replace("/")
+    Init();
+  }, [Init]);
+
+  return (
+    <>
+      <Box mt={10} pb={10} minH="100vh" display="flex" flexDirection="column">
+        <Breadcrumb.Root h="30px" w="90%" mx="auto">
+          <Breadcrumb.List>
+            <Breadcrumb.Item>
+              <Breadcrumb.Link href="/">
+                <LuHouse size="25px" />
+                <Text fontSize="17px">Home</Text>
+              </Breadcrumb.Link>
+            </Breadcrumb.Item>
+          </Breadcrumb.List>
+        </Breadcrumb.Root>
+        <Center w="full">
+          {isLoading ? (
+            <Center w="full">
+              <AnimationBox
+                w={"90%"}
+                dataState="open"
+                animationName="slide-from-bottom , fade-in"
+                animationDuration="500ms"
+              >
+                <Card.Root variant={"elevated"}>
+                  <Center p={10}>
+                    {retryFlag ? (
+                      <Text>
+                        시간이 걸립니다 잠시후에 다시 시도 부탁드립니다
+                      </Text>
                     ) : (
-                        <Card.Root variant={"elevated"} w={"90%"}>
-                            <Card.Body>
-                                <AnimationBox w={"100%"} dataState="open" animationName="slide-from-bottom , fade-in" animationDuration="500ms">
-                                    {
-                                        content ? (
-                                            <object
-                                                width={"100%"}
-                                                height={"1200px"}
-                                                data={URL.createObjectURL(content as Blob)}
-                                                type={content.type}
-                                            ></object>
-                                        ) : (
-                                            <Center gap={"20px"}>
-                                                <Icon size={"lg"} color={"red"}>
-                                                    <CiWarning />
-                                                </Icon>
-
-                                                <Heading>
-                                                    파일을 찾을 수 없습니다.
-                                                </Heading>
-                                            </Center>
-                                        )
-                                    }
-                                </AnimationBox>
-                            </Card.Body>
-                        </Card.Root>
+                      <Spinner size={"xl"} />
                     )}
-                </Center>
-            </Box >
-            <Box position="fixed" bottom="30px" right="30px">
-                <IconButton size="2xl" rounded="full" bg="gray.200" color="black" _hover={{ bg: "gray.400" }} onClick={() => {
-                    handleDownload();
-                }}>
-                    {!isDownload ? (
-                        <LuDownload size="30px" />
-                    ) : <Spinner size="md" />
-                    }
-                </IconButton>
-            </Box>
-        </>
-    );
+                  </Center>
+                </Card.Root>
+              </AnimationBox>
+            </Center>
+          ) : (
+            <Card.Root variant={"elevated"} w={"90%"}>
+              <Card.Body>
+                <AnimationBox
+                  w={"100%"}
+                  dataState="open"
+                  animationName="slide-from-bottom , fade-in"
+                  animationDuration="500ms"
+                >
+                  {content ? (
+                    <object
+                      width={"100%"}
+                      height={"1200px"}
+                      data={URL.createObjectURL(content as Blob)}
+                      type={content.type}
+                    ></object>
+                  ) : (
+                    <Center gap={"20px"}>
+                      <Icon size={"lg"} color={"red"}>
+                        <CiWarning />
+                      </Icon>
+
+                      <Heading>파일을 찾을 수 없습니다.</Heading>
+                    </Center>
+                  )}
+                </AnimationBox>
+              </Card.Body>
+            </Card.Root>
+          )}
+        </Center>
+      </Box>
+      <Box position="fixed" bottom="30px" right="30px">
+        <IconButton
+          size="2xl"
+          rounded="full"
+          bg="gray.200"
+          color="black"
+          _hover={{ bg: "gray.400" }}
+          onClick={() => {
+            handleDownload();
+          }}
+        >
+          {!isDownload ? <LuDownload size="30px" /> : <Spinner size="md" />}
+        </IconButton>
+      </Box>
+    </>
+  );
 };
 
 export default ResultPage;
