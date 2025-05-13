148cd813cbfaf2185e3ea70074989fd35529daaf
Kadrick <kbk2581553@gmail.com>
2025-05-13 15:14:37 +0900

    add mode

--- src/api/result/postFileByRequestId.tsx
+++ src/api/result/postFileByRequestId.tsx
@@ -4,40 +4,47 @@ import { toaster } from "~/components/ui/toaster";
 import HandleError from "~/common/HandleError";
 
 const postFileByRequestId = async (
-  file: File | undefined,
-  options: CheckElementList[],
-  githubLink: string
+    file: File | undefined,
+    options: CheckElementList[],
+    githubLink: string
 ): Promise<string> => {
-  try {
-    const body = new FormData();
-    if (file) {
-      body.append("project_file", file);
-    } else {
-      body.append("project_file", "");
-    }
-    const newOptions = options.reduce((acc, option) => {
-      acc.push(option.name);
-      return acc;
-    }, [] as string[]);
-    body.append("summary_options", JSON.stringify(newOptions));
-    body.append("github_url", githubLink);
-    const response = await axios.post(`${Config.API.Server}`, body);
+    try {
+        const body = new FormData();
+        if (file) {
+            body.append("project_file", file);
+        } else {
+            body.append("project_file", "");
+        }
+        const newOptions = options.reduce((acc, option) => {
+            acc.push(option.name);
+            return acc;
+        }, [] as string[]);
+        body.append("summary_options", JSON.stringify(newOptions));
+        body.append("github_url", githubLink);
 
-    const data = await response.data;
-    console.log(data.request_id);
-    return data.request_id;
-  } catch (error) {
-    if (isAxiosError(error)) {
-      HandleError(error);
-    } else {
-      toaster.create({
-        type: "error",
-        title: "post file error",
-        description: "파일 업로드에 실패하였습니다.",
-      });
+        if (githubLink) {
+            body.append("mode", "Github");
+        } else {
+            body.append("mode", "Zip");
+        }
+
+        const response = await axios.post(`${Config.API.Server}`, body);
+
+        const data = await response.data;
+        console.log(data.request_id);
+        return data.request_id;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            HandleError(error);
+        } else {
+            toaster.create({
+                type: "error",
+                title: "post file error",
+                description: "파일 업로드에 실패하였습니다.",
+            });
+        }
+        return Promise.reject("post file failed");
     }
-    return Promise.reject("post file failed");
-  }
 };
 
 export default postFileByRequestId;
